424d9d967734ad7984001cb11ee9e7a51bb055a7
Jagdsh lk Chand <jagdshlkchand@INJagdsh.local>
2018-08-29 07:51:29 +0530

    [SVA-004] Seperating login and payment Page <Jagdsh>

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,18 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET payment page. */
+router.get('/payment', function(req, res, next) {
+	res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': "*"})
+	fs.readFile('./views/payment.html', null, function(error, data){
+		if (error){
+			res.writeHead(404);
+			res.write('File not found');
+		} else {
+			res.write(data)
+		}
+		res.end();
+	});
+  });
+
 module.exports = router;
